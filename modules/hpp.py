7d226314dbc66243e3c99665ddecbe48febcab49
dismoralic <dismoralic@gmail.com>
2025-03-09 12:24:15 +0500

    Delete modules/hpp.py

--- modules/hpp.py
+++
@@ -1,6 +0,0 @@
-from telethon import events
-
-def register(client):
-    @client.on(events.NewMessage(pattern=r"\.hpp", outgoing=True))
-    async def hpp_handler(event):
-        await event.reply("test2")
