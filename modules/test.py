7706f13cf43fff081592501f59ff62af190c1508
dismoralic <dismoralic@gmail.com>
2025-03-09 12:19:20 +0500

    Delete modules directory

--- modules/test.py
+++
@@ -1,7 +0,0 @@
-from telethon import events
-
-async def test_command(event):
-    await event.edit("👍")
-
-def register(client):
-    client.add_event_handler(test_command, events.NewMessage(pattern=r"^\.test$", outgoing=True))
